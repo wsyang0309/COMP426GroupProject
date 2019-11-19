d3e4e4c24ba88d7591b51cc01b6d0ccd77e07c74
Ernesto Diaz <Ernesto@ernestos-mbp.wireless-psk.unc.edu>
2019-11-19 18:22:07 -0500

    testing

--- comp426-frontend/script.js
+++ comp426-frontend/script.js
@@ -1,26 +1,27 @@
 import apicalypse from 'apicalypse';
 import axios from 'axios';
- 
+
 let API_KEY = "ea8883099be2c2944a4d34a9752d94f0";
- 
- 
+
+
 let getGames = async function () {
-   console.log("Entered");
-   axios({
-       url: "https://api-v3.igdb.com/games",
-       method: 'POST',
-       headers: {
-           'Accept': 'application/json',
-           'user-key': API_KEY
-       },
-       
-       data: "fields *;"
-   })
-       .then(response => {
-           console.log(response.data);
-       })
-       .catch(err => {
-           console.error(err);
-       });
+    console.log("Entered");
+    axios({
+        url: "https://api-v3.igdb.com/games",
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'user-key': API_KEY
+        },
+
+        data: "fields *;"
+    })
+        .then(response => {
+            console.log(response.data);
+        })
+        .catch(err => {
+            console.error(err);
+        });
 }
+
 getGames();
