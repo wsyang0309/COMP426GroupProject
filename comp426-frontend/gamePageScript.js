5d7ba91cc4e563c009dad576f8ef5e515630c9a9
Ernesto Diaz <edog5723@gmail.com>
2019-11-20 18:10:55 -0500

    Generates individual game page given gameID

--- comp426-frontend/gamePageScript.js
+++ comp426-frontend/gamePageScript.js
@@ -3,11 +3,11 @@
 
 //API Documentation: https://api-docs.igdb.com/#about
 
-let gameID = 551;
+let gameID = 103329;
+const API_KEY = "ea8883099be2c2944a4d34a9752d94f0";
 
+let start = async function () {
 
-let getGame = async function () {
-    let API_KEY = "ea8883099be2c2944a4d34a9752d94f0";
     let result = await axios({
         url: "https://cors-anywhere.herokuapp.com/https://api-v3.igdb.com/games",
         method: 'POST',
@@ -16,31 +16,194 @@ let getGame = async function () {
             'user-key': API_KEY
         },
 
-        data: `fields name, age_ratings, cover, collection, popularity, rating , release_dates, summary, videos, websites; where id = ${gameID};`
+        data: `fields artworks , similar_games, name, age_ratings, cover, popularity, rating , storyline, first_release_date, summary, videos; where id = ${gameID};`
     })
     renderGame(result.data);
-
+    $('title').html(`${result.data[0].name}`);
 }
 
 let renderGame = async function (data) {
     let game = {
+        id: data[0].id,
         name: data[0].name,
+        artworks: data[0].artworks,
+        similar_games: data[0].similar_games,
         age_ratings: data[0].age_ratings,
         cover: data[0].cover,
-        collection: data[0].collection,
         popularity: data[0].popularity,
         rating: data[0].rating,
-        release_dates: data[0].release_dates,
+        first_release_date: data[0].first_release_date,
         summary: data[0].summary,
         videos: data[0].videos,
-        websites: data[0].websites
+        storyline: data[0].storyline,
     }
     console.log(game);
-    $("#gameInfo").append(`<div>${game.name}</div>`);
+
+    addCover(game.cover);
+    createDetails(game);
+    createSimilarGames(game.similar_games)
+    addArtwork();
+    addVideos();
+    addReviews();
+}
+let addReviews = async function () {
+
+    let result = await axios({
+        url: "https://cors-anywhere.herokuapp.com/https://api-v3.igdb.com/private/reviews",
+        method: "POST",
+        headers: {
+            "Accept": "application/json",
+            "user-key": API_KEY,
+
+        },
+        data: `fields conclusion,content,created_at,game,likes,title,user,user_rating,views;limit 50; where game=${gameID};`
+    })
+
+    let createReviews = async function (item) {
+
+        let getRating = await axios({
+            url: "https://cors-anywhere.herokuapp.com/https://api-v3.igdb.com/private/rates",
+            method: "POST",
+            headers: {
+                "Accept": "application/json",
+                "user-key": API_KEY
+            },
+            data: `fields rating ;limit 50; where id=${item.user_rating};`
+        })
+        let rating = getRating.data[0].rating;
+
+        let review = `
+        <div class="reviewBox">
+        <p class="subtitle">Review Title: ${item.title}</p>
+        <p>My Rating: ${rating}</p>
+        <p>Times liked: ${item.likes}</p>
+        <p>Date Reviewed: ${fixDate(item.created_at)}</p>
+
+        <p>The Review: </p>
+        <p>${item.content}</p>
+        </div>`
+        return review;
+    }
+
+    for (let i = 0; i < result.data.length; i++) {
+        let reviewHTML = await createReviews(result.data[i]);
+        $('#reviews').append(reviewHTML);
+    }
 }
 
 
+let addVideos = async function () {
+    let result = await axios({
+        url: "https://cors-anywhere.herokuapp.com/https://api-v3.igdb.com/game_videos",
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'user-key': API_KEY
+        },
+        data: `fields game,name,video_id; where game=${gameID};`
+    })
+
+
+    for (let i = 0; i < result.data.length; i++) {
+        let video = `<iframe width="504" height="378" class="column" allowfullscreen
+        src="https://www.youtube.com/embed/${result.data[i].video_id}">
+        </iframe>`
+        $('#videos').append(video);
+
+    }
+
+
+}
+let addCover = async function (id) {
+
+    let result = await axios({
+        url: "https://cors-anywhere.herokuapp.com/https://api-v3.igdb.com/covers",
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'user-key': API_KEY
+        },
+        data: `fields url, height, width, image_id; where id=${id};`
+    })
+    $('#cover').prepend(`<img src="//images.igdb.com/igdb/image/upload/t_1080p/${result.data[0].image_id}.jpg" class="media" />`)
+}
+
+let createDetails = function (game) {
+    let details =
+        `
+    <div class="myBox">
+    <p class="subtitle">${game.name}</p>
+    <p>Popularity: ${Math.round(game.popularity)}</p>
+    <p>Critics Rating: ${Math.round(game.rating)}</p>
+    <p>Release Date: ${fixDate(game.first_release_date)}</p>
+    <p>Summary: </p>
+    <p>${game.summary}</p>
+    <p>Plot: </p>
+    <p>${game.storyline}</p>
+    </div>
+    
+    
+    
+    `
+    $("#details").append(details);
+}
+let createSimilarGames = async function (games) {
+    let names = [];
+
+    for (let i = 0; i < games.length; i++) {
+        let result = await axios({
+            url: "https://cors-anywhere.herokuapp.com/https://api-v3.igdb.com/games",
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'user-key': API_KEY
+            },
+
+            data: `fields name; where id=${games[i]};`
+        })
+        names.push(result.data[0].name);
+    }
+
+
+    let createList = function (list) {
+        let ret = `<div class="myBox"><p class="subtitle">List of Similar Games</p><ol>`;
+
+        for (let i = 0; i < list.length; i++) {
+            ret += `<li>${list[i]}</li>`
+        }
+        ret += "</ol></div>"
+        return ret;
+    }
+    $("#similar").append(createList(names));
+}
+let fixDate = function (unix_timestamp) {
+    // Create a new JavaScript Date object based on the timestamp
+    let date = new Date(unix_timestamp * 1000);
+    let month = date.getMonth();
+    let day = date.getDay() + 1;
+    let year = date.getFullYear();
+    let formattedDate = month + '/' + day + '/' + year;
+    return formattedDate;
+}
+
+let addArtwork = async function () {
+    let result = await axios({
+        url: "https://cors-anywhere.herokuapp.com/https://api-v3.igdb.com/artworks",
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'user-key': API_KEY
+        },
+        data: `fields alpha_channel,animated,game,height,image_id,url,width; where game=${gameID};`
+    })
+
+    for (let i = 0; i < result.data.length; i++) {
+        let image = `<img src="//images.igdb.com/igdb/image/upload/t_720p/${result.data[i].image_id}.jpg" class="column pics" />`
+        $('#artwork').append(image);
+
+    }
+}
 
 $(document).ready(function () {
-    getGame();
+    start();
 });
