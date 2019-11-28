9bfef4f2476280c94869bdeb95ca07b0ae801619
SuyangW <wsyang0309@gmail.com>
2019-11-28 15:59:55 -0500

    simple version of user login & signup

--- comp426-frontend/indexScript.js
+++ comp426-frontend/indexScript.js
@@ -11,24 +11,39 @@ async function handleLogin(e) {
     let email = $("#email").val();
     let password = $("#password").val();
     let $message = $('#message');
+    let $nav_list = $('#nav_list');
 
     const result = await accountRoot.post('/login', {
         "name": email,
         "pass": password
     }).then(() => {
         $message.html('<span class="has-text-success">Success! You are now logged in.</span>');
+        $nav_list.append('<li><a href="homePage.html">Homepage</a></li>');
     }).catch(() => {
         $message.html('<span class="has-text-danger">Check your username and password again. Sign up if you have not done yet.</span>');
     });
-    console.log(result);
 }
 
 async function handleCreateAccount(e) {
     let email = $("#email").val();
     let password = $("#password").val();
+    let fname = $("#fname").val();
+    let lname = $("#lname").val();
+    let $message = $('#message');
+    let $nav_list = $('#nav_list');
+
     const result = await accountRoot.post('/create', {
         "name": email,
-        "pass": password
+        "pass": password,
+        "data": {
+            "fname": fname,
+            "lname": lname,
+        }
+    }).then(() => {
+        $message.html('<span class="has-text-success">Success! You are now signed up. You can now direct to our home page.</span>');
+        $nav_list.empty();
+        $nav_list.append('<li><a href="homePage.html">Homepage</a></li>');
+    }).catch(() => {
+        $message.html('<span class="has-text-danger">* indicates required items. Make sure your sign-up information are typed in correctly.</span>');
     });
-    console.log(result);
 }
